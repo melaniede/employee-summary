86fff476e6df38381d9136ece377f31884c36d97
melaniede <debarrosmelanie@gmail.com>
2020-10-12 23:04:35 -0400

    NPM Install and Employee class

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+let employees = [];
+
+// Prompt user questions
+let promptQuestions = () => {
+    return inquirer.prompt(questions[type]);
+};
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
